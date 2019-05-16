06f058ca03ecf50b97a855745315b126a3c6d690
Tobias Turke <Tobias.Turke@comparex.de>
2019-05-16 12:50:41 +0200

    finished json builder

--- Scripts/Json/Builder.py
+++ Scripts/Json/Builder.py
@@ -33,41 +33,73 @@ class Builder():
             message = template.format(type(ex).__name__, ex.args)
             print(message)
 
-
-    #TODO: Hier steht noch arbeit an. Beachte laut solid principles nicht zu extrem verschachteln! Eher single responsibility!
-
     def NewSentences(self, sentences:list):
-        sentences_json = {}
-        for index_s in range(sentences):
-            if isinstance(sentences[index_s], str):
-                sentences_json["S_"+index_s+1] = sentences[index_s]
-        return sentences_json
+        """
+        This method returns a json applyable dictionairy of sentence entries.
+            :param sentences:list: list of sentences
+        """   
+        try:
+            sentences_json = {}
+            for index_s in range(sentences):
+                if isinstance(sentences[index_s], str):
+                    sentences_json["S_"+index_s+1] = sentences[index_s]
+            return sentences_json
+        except Exception as ex:
+            template = "An exception of type {0} occurred in [JsonBuilder.NewSentences]. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
 
     def NewCategories(self, categories:list):
-        categories_json = {}
-        for category in categories:
-            if isinstance(category, Category):
-                categories_json[category.GetName()] = self.NewSentences(category.GetSentences())
+        """
+        This method returns a json applyable dictionairy of category object entries.
+            :param categories:list: list of category objects
+        """   
+        try:
+            categories_json = {}
+            for category in categories:
+                if isinstance(category, Category):
+                    categories_json[category.GetName()] = self.NewSentences(category.GetSentences())
 
-        return categories_json
+            return categories_json
+        except Exception as ex:
+            template = "An exception of type {0} occurred in [JsonBuilder.NewCategories]. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
 
     def NewWords(self, words:list):
-        word_json = {}
-        for word in words:
-            if isinstance(word, Word):
-                word_json[word.GetName()] = self.NewCategories(word.GetCategories()) 
-        return word_json
+        """
+        This method returns a json applyable dictionairy of word object entries. 
+            :param words:list: list of word objects
+        """
+        try:
+            word_json = {}
+            for word in words:
+                if isinstance(word, Word):
+                    word_json[word.GetName()] = self.NewCategories(word.GetCategories()) 
+            return word_json
+        except Exception as ex:
+            template = "An exception of type {0} occurred in [JsonBuilder.NewWords]. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
 
     def WriteToJson(self, words:dict):
-        data = None
-        
-        with open(self.file_name, "r+") as json_file:
-            data = json.load(json_file)
+        """
+        This method writes a json applyable dictionairy of word objects to a json file.
+            :param words:dict: dictionairy of word objects
+        """
+        try:
+            data = None
+            
+            with open(self.file_name, "r+") as json_file:
+                data = json.load(json_file)
 
-            for word_name, word in words.items():
-                if isinstance(word, Word) and isinstance(word_name, str):
-                    data[self._json_name] = word 
+                for word_name, word in words.items():
+                    if isinstance(word, Word) and isinstance(word_name, str):
+                        data[self._json_name] = word 
 
-        with open("dataset.json","w+") as json_file:
-            json.dump(data, json_file)
-
+            with open(self.file_name,"w+") as json_file:
+                json.dump(data, json_file)
+        except Exception as ex:
+            template = "An exception of type {0} occurred in [JsonBuilder.WriteToJson]. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
