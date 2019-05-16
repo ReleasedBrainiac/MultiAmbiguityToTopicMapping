946f3231430cd60ac2aec80da517ed0ac29c8724
Tobias Turke <Tobias.Turke@comparex.de>
2019-05-16 12:41:27 +0200

    renaming probs

--- Scripts/Json/JsonBuilder.py
+++ Scripts/Json/JsonBuilder.py
@@ -1,6 +1,7 @@
 import json
 import os
 from SupportMethods.ContentSupport import isNotNone
+from Scripts.Models.DataModels import Word, Category
 
 class Builder():
 
@@ -27,7 +28,7 @@ class Builder():
             with open(self._file_name, "w+") as file:
                 init_json = "{\""+self._json_name+"\": {}}"
                 json.dump(json.loads(init_json), file)
-            except Exception as ex:
+        except Exception as ex:
             template = "An exception of type {0} occurred in [JsonBuilder.InitJson]. Arguments:\n{1!r}"
             message = template.format(type(ex).__name__, ex.args)
             print(message)
@@ -35,23 +36,44 @@ class Builder():
 
     #TODO: Hier steht noch arbeit an. Beachte laut solid principles nicht zu extrem verschachteln! Eher single responsibility!
 
-    def createEntry(self, dataList:list):
-        json_data = None
-        with open(self._file_name, "r+") as json_file:
-            json_data = json.load(json_file)    
-        for data in dataList:
-            print(data)
+    def NewSentences(self, sentences:list):
+        sentences_json = {}
+        for index_s in range(sentences):
+            if isinstance(sentences[index_s], str):
+                sentences_json["S_"+index_s+1] = sentences[index_s]
+        return sentences_json
+
+    def NewCategories(self, categories:list):
+        categories_json = {}
+        for category in categories:
+            if isinstance(category, Category):
+                categories_json[category.GetName()] = self.NewSentences(category.GetSentences())
+
+        return categories_json
+
+    def NewWords(self, words:list):
+        word_json = {}
+        for word in words:
+            if isinstance(word, Word):
+                word_json[word.GetName()] = self.NewCategories(word.GetCategories()) 
+        return word_json
+
+    def WriteToJson(self, words:dict):
+        data = None
+        
+        with open(self.file_name, "r+") as json_file:
+            data = json.load(json_file)
+
+            for word_name, word in words.items():
+                if isinstance(word, Word) and isinstance(word_name, str):
+                    data[self._json_name] = word 
+
         with open("dataset.json","w+") as json_file:
             json.dump(data, json_file)
 
 
 
-    def buildSubcategorie(self, sentences:list):
-        subcategorie={}
-        for i in range(sentences):
-            subcategorie["sens"+i]=sentences[i]
-        return subcategorie
-        
+
 
     def newEntry(self):
         subcategorie ={}
@@ -81,6 +103,7 @@ class Builder():
             data = json.load(json_file)
             data["Dataset"]["baum"]= categorie 
             data["Dataset"]["brine"]=categorie
+
         with open("dataset.json","w+") as json_file:
             json.dump(data, json_file)
 
