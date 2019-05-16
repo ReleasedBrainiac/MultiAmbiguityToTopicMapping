d8ba30b1fac9d66c5dc588ed300e287fb06d3234
Tobias Turke <Tobias.Turke@comparex.de>
2019-05-16 12:42:52 +0200

    new pipes and switches implemented

--- Scripts/main.py
+++ Scripts/main.py
@@ -12,11 +12,13 @@ from Json.Builder import Builder
 from FileManager.FileWriter import Writer
 from FileManager.FileReader import Reader
 from FileManager.UniLeipzigApiCaller import UniLeipzigAPICaller
+from Models.Enums import Process
+from Models.DataModels import Word
 
 class AmbiguityMapper():
 
     # For pulling raw data samples
-    PULL_DATASET:bool = False
+    _process:Process = Process.DATA_TO_JSON
     COLLECT_WORD_LIST_PATH:str = "polysem.txt"
     COLLECTING_LIMIT:int = 30
     COLLECT_API_BASE_URL:str = "http://api.corpora.uni-leipzig.de/ws/sentences/"
@@ -25,6 +27,8 @@ class AmbiguityMapper():
     DATASET_PATH:str = "Dataset/"
     DATASET_RAW_PATH:str = DATASET_PATH + "Basis/"
     DATASET_SINGLE_FILE_TYP = "txt"
+    JSON_PATH = "dataset.json"
+    JSON_NAME = "dataset"
 
     
     def Execute(self):
@@ -52,10 +56,13 @@ class AmbiguityMapper():
             #print("Keras version: \t\t=> ", keras.__version__, '\n')
             print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
             
-            if self.PULL_DATASET:
-                self.ExecuteRawDatasetCollection()
-            else:
+            if self._process is Process.DATA_TO_NETWORK:
                 self.ExecuteANNProcessing()
+            elif self._process is Process.DATA_TO_JSON:
+                self.ExecuteDatasetToJson()
+            else:
+                self.ExecuteRawDatasetCollection()
+
         except Exception as ex:
             template = "An exception of type {0} occurred in [Main.ExecuteTool]. Arguments:\n{1!r}"
             message = template.format(type(ex).__name__, ex.args)
@@ -65,10 +72,26 @@ class AmbiguityMapper():
     def ExecuteANNProcessing(self):
         try:
             #TODO: Currently not in use since the pipe and the network are still missing.
-            builder = Builder("dataset.json", "dataset")
+            pass
 
         except Exception as ex:
             template = "An exception of type {0} occurred in [Main.ExecuteANNProcessing]. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
+    
+    def ExecuteDatasetToJson(self, json_path:str):
+        try:
+            builder:Builder = Builder(self.JSON_PATH, self.JSON_NAME)
+            words:list = []
+
+            for file_item in os.listdir(self.DATASET_PATH):
+                composite_path:str = self.DATASET_PAT + file_item
+                if file_item is str and os.path.exists(composite_path): 
+                    words.append(Word(file_item.replace(".txt","").lower(), Reader(composite_path).CategoriesReader()))
+
+            builder.NewWords(words)
+        except Exception as ex:
+            template = "An exception of type {0} occurred in [Main.ExecuteDatasetToJson]. Arguments:\n{1!r}"
             message = template.format(type(ex).__name__, ex.args)
             print(message)
 
