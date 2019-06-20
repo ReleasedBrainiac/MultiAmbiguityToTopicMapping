4f5c000cbfee1787c6c38e8011fe7d8c13014a8c
Tobias Turke <Tobias.Turke@comparex.de>
2019-06-20 16:30:11 +0200

    added tagged tuple generation for each sample. implemented tuple, tagged tuple generator and tagged tuple to dataset converter.

--- Scripts/Models/Samples.py
+++ Scripts/Models/Samples.py
@@ -31,6 +31,23 @@ class Sample(object):
             template = "An exception of type {0} occurred in [Sample.GetTuple]. Arguments:\n{1!r}"
             message = template.format(type(ex).__name__, ex.args)
             print(message)
+    
+    def GetTaggedTuple(self):
+        """
+        This method returns the tagged sample tuple if all values not None.
+        This is for Doc2Vec machine learning.
+        """   
+        try:    
+            if isNone(self._word) or isNone(self._category) or isNone(self._sentence):
+                return None
+            else:
+                return (self._word + "_" + self._category, self._sentence)
+        except Exception as ex:
+            template = "An exception of type {0} occurred in [Sample.GetTaggedTuple]. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
+
+
 
 class SampleGenerator(object):
 
@@ -64,6 +81,43 @@ class SampleGenerator(object):
                 return None
         except Exception as ex:
             template = "An exception of type {0} occurred in [SampleGenerator.GenerateDatasetSamples]. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
+
+    def GenerateTuples(self):
+        """
+        This method generates 3 element tuples from the collected samples.
+        """
+        try:
+            return [v.GetTuple() for v in  self.GenerateDatasetSamples()]
+        except Exception as ex:
+            template = "An exception of type {0} occurred in [SampleGenerator.GenerateTuples]. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
+
+    def GenerateTaggedTuples(self):
+        """
+        This method generates 2 element tagged tuples from the collected samples.
+        The word and category dimension were merged together.
+        """
+        try:
+            return [v.GetTaggedTuple() for v in  self.GenerateDatasetSamples()]
+        except Exception as ex:
+            template = "An exception of type {0} occurred in [SampleGenerator.GenerateTaggedTuples]. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
+
+    def GenerateDocsAndLabels(self, samples:list = None):
+        """
+        This function generates 2 lists from 2D tuple list. [Docs, Labels]
+        """
+        try:
+            if len(samples[0]) == 2:
+                return zip(*samples)
+            else:
+                return None
+        except Exception as ex:
+            template = "An exception of type {0} occurred in [SampleGenerator.GenerateDocsAndLabels]. Arguments:\n{1!r}"
             message = template.format(type(ex).__name__, ex.args)
             print(message)
 
